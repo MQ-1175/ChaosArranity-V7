e3f763525ab66bde03b250ae957eec532d0eb600
MQ-1175 <85242532+MQ-1175@users.noreply.github.com>
2022-01-10 17:16:57 +0700

    Update ejectors.js

--- scripts/ejectors.js
+++ scripts/ejectors.js
@@ -78,3 +78,39 @@ const multithorium = multiLib.MultiCrafter(GenericSmelter, GenericSmelter.Smelte
       craftTime:75,
   },
 ]);
+
+const alloyRefinery = multiLib.MultiCrafter(GenericSmelter, GenericSmelter.SmelterBuild, "alloy-refinery",[
+    {
+      input:{
+          items:["copper/28","lead/35","titanium/14","silicon/30"],
+          liquids:["water/60"],
+          power:24.3333333333333333,
+      },
+      output:{
+          items:["surge-alloy/12"],
+      },
+      craftTime:600,
+  },
+  {
+    input:{
+        items:["copper/30","lead/37","titanium/15","silicon/33"],
+        liquids:["cryofluid/22"],
+        power:24.3333333333333333,
+    },
+    output:{
+        items:["surge-alloy/16"],
+    },
+    craftTime:480,
+  },
+  {
+    input:{
+        items:["copper/35","lead/40","titanium/19","silicon/38"],
+        liquids:["chaos-arranity-cryogenic-fluid/10"],
+        power:24.3333333333333333,
+    },
+    output:{
+        items:["surge-alloy/20"],
+    },
+    craftTime:300,
+  },
+]);
