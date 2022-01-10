b2597c2223a2f838c29149f4eb4f3e33e6efc298
MQ-1175 <85242532+MQ-1175@users.noreply.github.com>
2022-01-10 16:44:44 +0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,5 @@
-// Content
-require("content/defense");
-require("content/effect");
-require("content/distribution");
-require("content/liquid");
-require("content/power");
-require("content/production");
-require("content/turrets");
-require("content/units");
-require("content/items");
-require("content/bullets");
-require("content/units");
-require("content/weather");
-require("content/statusEffects");
-require("units");
-
-require("effects");
-require("techTree")
+require("abilities");
+require("launchpadadamantite");
+require("hyperwall");
+require("ejectors");
+require("odium-mine");
