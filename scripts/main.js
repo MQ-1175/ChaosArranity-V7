e5293f10d3a69b6aeb2ef6cf0ad120069d70307f
MQ-1175 <85242532+MQ-1175@users.noreply.github.com>
2022-01-10 21:33:21 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,5 @@ require("hyperwall");
 require("ejectors");
 require("odium-mine");
 
-require("content/blocks/units");
-require("content/units");
-require("content/weather");
-require("content/statusEffects");
-
 require("effects");
 require("techTree")
