1baf362e39c23f3a994f2e9f847cfd424ceb6ba6
MQ-1175 <85242532+MQ-1175@users.noreply.github.com>
2022-01-10 18:55:43 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -4,17 +4,7 @@ require("hyperwall");
 require("ejectors");
 require("odium-mine");
 
-require("content/blocks/crafting");
-require("content/blocks/defense");
-require("content/blocks/effect");
-require("content/blocks/distribution");
-require("content/blocks/liquid");
-require("content/blocks/power");
-require("content/blocks/production");
-require("content/blocks/turrets");
 require("content/blocks/units");
-require("content/items");
-require("content/bullets");
 require("content/units");
 require("content/weather");
 require("content/statusEffects");
